3b46368c5c2cf5edc275d327e61b70c9630ff55f
Edge <edge2347@gmail.com>
2020-08-30 02:46:46 +0100

    yes

--- app/api/main.py
+++ app/api/main.py
@@ -5,7 +5,7 @@ from app.api import bp
 from app.api.auth import token_auth
 from app.api.errors import bad_request
 
-@bp.route('/test')
+@bp.route('/test', methods=['GET'])
 def test():
     return jsonify({'status': 'yep'})
 
