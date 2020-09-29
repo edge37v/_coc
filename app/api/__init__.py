989c307db42ccdb376d0a058a2b2055b9ea339d6
Edward Gold Edem Hogan <edge3769@gmail.com>
2020-09-29 17:52:16 +0100

    bruh

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -2,4 +2,4 @@ from flask import Blueprint
 
 bp = Blueprint('api', __name__, url_prefix='/api')
 
-from app.api import blog, forums, auth, main, users, errors, paystack, tokens, learning
+from app.api import blog, forums, auth, users, errors, paystack, tokens, learning
